bd6482d372804d96b8e58bd68ca9fefc3ed0f9ff
Peninah98 <peninangizwenayo@gmail.com>
2025-12-09 11:26:46 +0200

    feat:create the web elements

--- src/test/java/base/BaseTests.java
+++ src/test/java/base/BaseTests.java
@@ -1,10 +1,13 @@
 package base;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import java.time.Duration;
-import java.util.concurrent.TimeUnit;
+import java.util.ArrayList;
+import java.util.List;
 
 public class BaseTests {
     private WebDriver driver ;
@@ -14,9 +17,23 @@ public class BaseTests {
         driver = new ChromeDriver();
         driver.get("https://the-internet.herokuapp.com");
         System.out.println(driver.getTitle());
-        driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(20));
-        driver.manage().window().fullscreen();
+//        driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(20));
+//        driver.manage().window().fullscreen();
+
+      WebElement inputLinks = driver.findElement(new By.ByLinkText("Shifting Content"));
+      inputLinks.click();
+      WebElement shiftingContent = driver.findElement(new By.ByLinkText("Example 1: Menu Element"));
+      shiftingContent.click();
+      List<WebElement> shiftingMenu = driver.findElements(new By.ByTagName("a"));
+      System.out.println(shiftingMenu.size());
+
+
         driver.quit();
+    }
+
+    public String clickLinks(String link){
+        driver.findElement(new By.ByLinkText(link));
+        return link;
     }
 
     public static  void main() {
